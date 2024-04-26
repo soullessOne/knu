4f282a9e72721736df23fa4dc5400220b6dfa6fe
Rustam Razabekov <rustamrazabekov1705@gmail.com>
2024-04-26 14:29:28 +0600

    First initialization

---
+++ src/main/java/rustam/proavn/ProavnApplication.java
@@ -0,0 +1,13 @@
+package rustam.proavn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProavnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProavnApplication.class, args);
+	}
+
+}
